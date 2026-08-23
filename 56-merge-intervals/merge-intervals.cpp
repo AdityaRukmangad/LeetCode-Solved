9e3ccd795a590da58e376e392d97a23c735b298e
Aditya Rukmangad <adityarukmangad11@gmail.com>
2026-08-24 01:14:34 +0530

    Time: 7 ms (38.64%) | Memory: 25 MB (6.85%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>merged;
+        vector<int>temp;
+
+        if(intervals.size()==0) return merged;
+
+        sort(intervals.begin(),intervals.end());
+
+        temp = (intervals[0]);
+
+        for(auto it : intervals){
+            if(it[0]<=temp [1])
+            temp[1] = max(temp[1],it[1]);
+
+            else{
+            merged.push_back(temp);
+            temp =it;
+            }
+        }
+        merged.push_back(temp);
+        return merged;
+    }
+};
